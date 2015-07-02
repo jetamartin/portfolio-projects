8b187514edc021f4554a686d7712c23f0a0c8ae6
Jet Martin <jetamartin@gmail.com>
2015-07-01 18:15:30 -0700

    Add blog truncate with elipsis using dotdotdog.js solution, added additional automation for portforlio and blog items

--- js/jet.js
+++ js/jet.js
@@ -77,6 +77,44 @@ $(document).ready(function() {
 //   });
 // });
 
+$(".truncateIt").dotdotdot({
+		// configuration goes here
+		/*	The text to add as ellipsis. */
+	ellipsis	: '... ',
+
+	/*	How to cut off the text/html: 'word'/'letter'/'children' */
+	wrap		: 'word',
+
+	/*	Wrap-option fallback to 'letter' for long words */
+	fallbackToLetter: true,
+
+	/*	jQuery-selector for the element to keep and put after the ellipsis. */
+	after		: 'a.next',
+
+	/*	Whether to update the ellipsis: true/'window' */
+	watch		: false,
+
+	/*	Optionally set a max-height, if null, the height will be measured. */
+	height		: 100,
+
+	/*	Deviation for the height-option. */
+	tolerance	: 0,
+
+	/*	Callback function that is fired after the ellipsis is added,
+		receives two parameters: isTruncated(boolean), orgContent(string). */
+	callback	: function( isTruncated, orgContent ) {},
+
+	lastCharacter	: {
+
+		/*	Remove these characters from the end of the truncated text. */
+		remove		: [ ' ', ',', ';', '.', '!', '?' ],
+
+		/*	Don't add an ellipsis if this array contains
+			the last character of the truncated text. */
+		noEllipsis	: []
+	}
+});
+
 // Scroll location for buttons on banner page
 
 $('a.scroll-link').on('click', function(e) {
